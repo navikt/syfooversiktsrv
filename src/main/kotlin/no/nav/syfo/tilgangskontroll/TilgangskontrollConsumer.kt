67bfc78b2eb59ce06a49f950cd18028a7c84b040
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-06-24 13:01:27 +0200

    Fjern test-logging

--- src/main/kotlin/no/nav/syfo/tilgangskontroll/TilgangskontrollConsumer.kt
+++ src/main/kotlin/no/nav/syfo/tilgangskontroll/TilgangskontrollConsumer.kt
@@ -3,9 +3,6 @@ package no.nav.syfo.tilgangskontroll
 import io.ktor.client.HttpClient
 import io.ktor.client.request.*
 import io.ktor.http.ContentType
-import org.slf4j.LoggerFactory
-
-val log: org.slf4j.Logger = LoggerFactory.getLogger("no.nav.syfo.tilgangskontroll")
 
 class TilgangskontrollConsumer(
         private val endpointUrl: String,
@@ -15,8 +12,6 @@ class TilgangskontrollConsumer(
     private val pathTilgangTilBruker = "/bruker"
 
     suspend fun harVeilederTilgangTilPerson(fnr: String, token: String): Boolean {
-        log.info("JTRACE: sjekkertilgang til {} for {} mot url {}", fnr, token, "$endpointUrl/syfo-tilgangskontroll/api/tilgang$pathTilgangTilBruker")
-
         val response = client.get<Tilgang>("$endpointUrl/syfo-tilgangskontroll/api/tilgang$pathTilgangTilBruker") {
             accept(ContentType.Application.Json)
             headers {
