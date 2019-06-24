2542d05153c1144571415b75c00991d2642fc0dc
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-06-24 12:40:33 +0200

    Legg til endepunkt for henting av personoversikt for enhet

--- src/main/kotlin/no/nav/syfo/personstatus/PersonTildelingService.kt
+++ src/main/kotlin/no/nav/syfo/personstatus/PersonTildelingService.kt
@@ -4,6 +4,7 @@ import no.nav.syfo.db.DatabaseInterface
 import no.nav.syfo.personstatus.domain.VeilederBrukerKnytning
 
 class PersonTildelingService(private val database: DatabaseInterface) {
+
     fun hentBrukertilknytningerPaVeileder(veilederIdent: String) = database.hentBrukereTilknyttetVeileder(veilederIdent).map { VeilederBrukerKnytning(it.veilederIdent, it.fnr, it.enhet) }
 
     fun lagreKnytningMellomVeilederOgBruker(veilederBrukerKnytninger: List<VeilederBrukerKnytning>) = veilederBrukerKnytninger.map { database.lagreBrukerKnytningPaEnhet(it) }
---
+++ src/main/kotlin/no/nav/syfo/personstatus/domain/PersonOversiktStatus.kt
@@ -0,0 +1,7 @@
+package no.nav.syfo.personstatus.domain
+
+data class PersonOversiktStatus(
+        val veilederIdent: String,
+        val fnr: String,
+        val enhet: String
+)
