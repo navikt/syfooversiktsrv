a600fe619467677e670b32af44c6950ffd0e1da3
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-07-09 11:16:59 +0200

    Filtrer vekk personer med behandlet motebehov

--- src/main/kotlin/no/nav/syfo/personstatus/PersonoversiktStatusService.kt
+++ src/main/kotlin/no/nav/syfo/personstatus/PersonoversiktStatusService.kt
@@ -5,4 +5,5 @@ import no.nav.syfo.db.DatabaseInterface
 class PersonoversiktStatusService(private val database: DatabaseInterface) {
 
     fun hentPersonoversiktStatusTilknyttetEnhet(enhet: String) = database.hentPersonerTilknyttetEnhet(enhet)
+            .filter { it.motebehovUbehandlet != false }
 }
