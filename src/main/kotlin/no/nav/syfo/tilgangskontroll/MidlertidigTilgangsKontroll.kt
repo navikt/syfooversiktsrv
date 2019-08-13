ed492787f1251b1011f9c3f870db18006eb97b23
Kristian Storvoll <kristian.storvoll@nav.no>
2019-08-13 12:47:49 +0200

    rename class

--- src/main/kotlin/no/nav/syfo/tilgangskontroll/MidlertidigTilgangsKontroll.kt
+++ src/main/kotlin/no/nav/syfo/tilgangskontroll/MidlertidigTilgangsKontroll.kt
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import no.nav.syfo.LOG
-import no.nav.syfo.auth.getVeilederTokenPayload
-import no.nav.syfo.isPreProd
 import no.nav.syfo.util.allToUpperCase
 import java.nio.file.Paths
 
@@ -15,14 +13,14 @@ private val objectMapper: ObjectMapper = ObjectMapper().apply {
     registerModule(JavaTimeModule())
 }
 
-data class TilgangsFil(
+data class Tilganger (
         val identer: List<String>
 )
 
-private fun lesTilgangsFil(path: String): TilgangsFil {
+private fun lesTilgangsfil(path: String): Tilganger {
     LOG.info("Leser tilgangsfil fra $path")
     val s = Paths.get(path).toFile().readText()
-    return objectMapper.readValue<TilgangsFil>(s).also { LOG.info("Leste tilgang fra fil med ${it.identer.size} identer") }
+    return objectMapper.readValue<Tilganger>(s).also { LOG.info("Leste tilgang fra fil med ${it.identer.size} identer") }
 }
 
 private const val vaultFile = "/var/run/secrets/nais.io/vault/tilganger.json"
@@ -30,9 +28,9 @@ private const val vaultFile = "/var/run/secrets/nais.io/vault/tilganger.json"
 class MidlertidigTilgangsSjekk(pathTilTilgangsfil: String = vaultFile) {
 
     var tilgangListe = arrayListOf<String>()
-    
+
     init {
-        val tilgangsFil = lesTilgangsFil(pathTilTilgangsfil)
+        val tilgangsFil = lesTilgangsfil(pathTilTilgangsfil)
         tilgangListe.addAll(tilgangsFil.identer.allToUpperCase())
     }
 
