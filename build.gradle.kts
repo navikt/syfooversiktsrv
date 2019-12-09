78142198a06e0ef6aba0a3b9f4382f843d767716
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-12-09 12:40:29 +0100

    Bump Kotlin 1.3.50 -> 1.3.60

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ tasks.withType<Jar> {
 }
 
 plugins {
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.60"
     id("com.diffplug.gradle.spotless") version "3.18.0"
     id("com.github.johnrengelman.shadow") version "4.0.4"
 }
