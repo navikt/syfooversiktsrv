ceec675f1f43a0756db8f844bdf2eb31d50ef011
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-02-24 14:25:42 +0100

    Remove duplicate prometheus-endpoint

--- src/main/kotlin/no/nav/syfo/api/Pod.kt
+++ src/main/kotlin/no/nav/syfo/api/Pod.kt
@@ -1,19 +1,14 @@
 package no.nav.syfo.api
 
 import io.ktor.application.call
-import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respondText
-import io.ktor.response.respondTextWriter
 import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.prometheus.client.CollectorRegistry
-import io.prometheus.client.exporter.common.TextFormat
 import no.nav.syfo.ApplicationState
 
 fun Routing.registerPodApi(
-        applicationState: ApplicationState,
-        collectorRegistry: CollectorRegistry = CollectorRegistry.defaultRegistry
+        applicationState: ApplicationState
 ) {
     get("/is_alive") {
         if (applicationState.running) {
@@ -27,12 +22,6 @@ fun Routing.registerPodApi(
             call.respondText("I'm ready! :)")
         } else {
             call.respondText("Please wait! I'm not ready :(", status = HttpStatusCode.InternalServerError)
-        }
-    }
-    get("/prometheus") {
-        val names = call.request.queryParameters.getAll("name[]")?.toSet() ?: setOf()
-        call.respondTextWriter(ContentType.parse(TextFormat.CONTENT_TYPE_004)) {
-            TextFormat.write004(this, collectorRegistry.filteredMetricFamilySamples(names))
         }
     }
 }
