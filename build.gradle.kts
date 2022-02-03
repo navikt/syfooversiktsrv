17c5ad3e7b5254595e8b82a0ebb810a23cf920ee
Erik Jansen <erik.gunnar.jansen@nav.no>
2022-02-03 14:50:04 +0100

    Undo upgrade of PostgresEmbedded
    Undo upgrade due to issues with Docker on Windows OS when runings tests locally.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ object Versions {
     const val mockk = "1.12.2"
     const val micrometerRegistry = "1.8.2"
     const val nimbusjosejwt = "9.18"
-    const val postgresEmbedded = "1.0.0"
+    const val postgresEmbedded = "0.13.4"
     const val postgres = "42.3.2"
     const val spek = "2.0.17"
 }
