0e20ce41b5a31b5510cd2769f59aeb791cb63d86
geir-waagboe <geir.arne.waagbo@nav.no>
2022-06-16 09:42:38 +0200

    Add specific index for enhetens oversikt

---
+++ src/main/resources/db/migration/V12_4__add_index_enhetens_oversikt.sql
@@ -0,0 +1,3 @@
+CREATE INDEX IX_PERSON_OVERSIKT_STATUS_ENHETENS_OVERSIKT
+ON PERSON_OVERSIKT_STATUS (tildelt_enhet, dialogmotekandidat_generated_at)
+WHERE (motebehov_ubehandlet OR moteplanlegger_ubehandlet OR oppfolgingsplan_lps_bistand_ubehandlet OR dialogmotekandidat);
