b307292bef896341e435785963a0ff1503ff9cac
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2022-10-05 13:02:21 +0200

    Upgrade to postgres 14 in preprod (#160)

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -4,3 +4,9 @@ GRANT SELECT ON ALL TABLES IN SCHEMA public TO "isyfo-analyse";
 -- GRANT SELECT, UPDATE ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 -- GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 -- GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO cloudsqliamuser;
+
+DO $$
+BEGIN
+    ANALYZE;
+END
+$$;
