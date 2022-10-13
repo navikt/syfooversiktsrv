590ca5c6f0e75f245232174f1f52fb2e29afe042
geir-waagboe <geir.arne.waagbo@nav.no>
2022-10-13 12:52:39 +0200

    Expire kandidatstatus when new oppfolgingstilfelle

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -2,11 +2,5 @@ GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO "isyfo-analyse";
 
 -- GRANT SELECT, UPDATE ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
--- GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 -- GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO cloudsqliamuser;
-
-DO $$
-BEGIN
-    ANALYZE;
-END
-$$;
