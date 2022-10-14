6af7b5bf6bcddc21810395cd11f2fb3929e38702
geir-waagboe <geir.arne.waagbo@nav.no>
2022-10-14 15:44:29 +0200

    Set as kandidat only after seven days

--- src/test/kotlin/no/nav/syfo/testutil/TestEnvironment.kt
+++ src/test/kotlin/no/nav/syfo/testutil/TestEnvironment.kt
@@ -68,7 +68,7 @@ fun testEnvironment(
     ),
     redis = RedisEnvironment(
         host = "localhost",
-        port = 6371,
+        port = 6379,
         secret = "password",
     ),
     serviceuserUsername = "",
