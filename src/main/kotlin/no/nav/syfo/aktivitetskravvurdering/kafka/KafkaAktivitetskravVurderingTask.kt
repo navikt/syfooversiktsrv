c656b1b2cf8acfb25ff64a2847793cfb19b3623f
John Martin Lindseth <john.martin.lindseth@nav.no>
2022-11-16 14:29:00 +0100

    Consume aktivitetskrav-vurdering topic.
    Turned off with toggled.
    Nothing is done with the record yet.

---
+++ src/main/kotlin/no/nav/syfo/aktivitetskravvurdering/kafka/KafkaAktivitetskravVurderingTask.kt
@@ -0,0 +1,39 @@
+package no.nav.syfo.aktivitetskravvurdering.kafka
+
+import no.nav.syfo.application.ApplicationState
+import no.nav.syfo.application.database.database
+import no.nav.syfo.application.kafka.KafkaEnvironment
+import no.nav.syfo.application.kafka.kafkaAivenConsumerConfig
+import no.nav.syfo.kafka.launchKafkaTask
+import no.nav.syfo.util.configuredJacksonMapper
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.Deserializer
+import java.util.*
+
+const val AKTIVITETSKRAV_VURDERING_TOPIC = "teamsykefravr.aktivitetskrav-vurdering"
+
+fun launchKafkaTaskAktivitetskravVurdering(
+    applicationState: ApplicationState,
+    kafkaEnvironment: KafkaEnvironment,
+) {
+    val kafkaAktivitetskravVurderingService = KafkaAktivitetskravVurderingService(
+        database = database,
+    )
+    val consumerProperties = Properties().apply {
+        putAll(kafkaAivenConsumerConfig(kafkaEnvironment = kafkaEnvironment))
+        this[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = KafkaAktivitetskravVurderingDeserializer::class.java
+    }
+
+    launchKafkaTask(
+        applicationState = applicationState,
+        topic = AKTIVITETSKRAV_VURDERING_TOPIC,
+        consumerProperties = consumerProperties,
+        kafkaConsumerService = kafkaAktivitetskravVurderingService,
+    )
+}
+
+class KafkaAktivitetskravVurderingDeserializer : Deserializer<KafkaAktivitetskravVurdering> {
+    private val mapper = configuredJacksonMapper()
+    override fun deserialize(topic: String, data: ByteArray): KafkaAktivitetskravVurdering =
+        mapper.readValue(data, KafkaAktivitetskravVurdering::class.java)
+}
