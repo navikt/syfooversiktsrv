c3b5dd51698ce265f90aa30eab9ef1b3d2527cb2
Eirik Dahlen <dahleneirik@gmail.com>
2022-11-23 15:00:42 +0100

    Add test for service

---
+++ src/test/kotlin/no/nav/syfo/testutil/generator/KafkaIdenthendelseDTOGenerator.kt
@@ -0,0 +1,42 @@
+package no.nav.syfo.testutil.generator
+
+import no.nav.syfo.domain.PersonIdent
+import no.nav.syfo.identhendelse.kafka.IdentType
+import no.nav.syfo.identhendelse.kafka.Identifikator
+import no.nav.syfo.identhendelse.kafka.KafkaIdenthendelseDTO
+import no.nav.syfo.testutil.UserConstants
+
+fun generateKafkaIdenthendelseDTOGenerator(
+    personident: PersonIdent = PersonIdent(UserConstants.ARBEIDSTAKER_FNR),
+    hasOldPersonident: Boolean,
+): KafkaIdenthendelseDTO {
+    val identifikatorer = mutableListOf(
+        Identifikator(
+            idnummer = personident.value,
+            type = IdentType.FOLKEREGISTERIDENT,
+            gjeldende = true,
+        ),
+        Identifikator(
+            idnummer = "10$personident",
+            type = IdentType.AKTORID,
+            gjeldende = true
+        ),
+    )
+    if (hasOldPersonident) {
+        identifikatorer.addAll(
+            listOf(
+                Identifikator(
+                    idnummer = UserConstants.ARBEIDSTAKER_2_FNR,
+                    type = IdentType.FOLKEREGISTERIDENT,
+                    gjeldende = false,
+                ),
+                Identifikator(
+                    idnummer = "9${UserConstants.ARBEIDSTAKER_2_FNR.drop(1)}",
+                    type = IdentType.FOLKEREGISTERIDENT,
+                    gjeldende = false,
+                ),
+            )
+        )
+    }
+    return KafkaIdenthendelseDTO(identifikatorer)
+}
