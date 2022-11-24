379976212731f8e49e855d4d9d52664a7841b14c
John Martin Lindseth <john.martin.lindseth@nav.no>
2022-11-24 16:09:03 +0100

    Update index for enhetens oversikt with new aktivitetskrav column

---
+++ src/main/resources/db/migration/V12_7__update_index_enhetens_oversikt_aktivitetskrav.sql
@@ -0,0 +1,5 @@
+DROP INDEX IX_PERSON_OVERSIKT_STATUS_ENHETENS_OVERSIKT;
+
+CREATE INDEX IX_PERSON_OVERSIKT_STATUS_ENHETENS_OVERSIKT
+ON PERSON_OVERSIKT_STATUS (tildelt_enhet, dialogmotekandidat_generated_at)
+WHERE (motebehov_ubehandlet OR moteplanlegger_ubehandlet OR oppfolgingsplan_lps_bistand_ubehandlet OR dialogmotekandidat OR (aktivitetskrav = 'NY' OR aktivitetskrav = 'AVVENT'));
