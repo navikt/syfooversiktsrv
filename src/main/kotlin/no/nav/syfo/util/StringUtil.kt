ccd254ae9ec7790b2643aca64582319671ead159
Eirik Dahlen <37441744+eirikdahlen@users.noreply.github.com>
2022-12-09 10:00:47 +0100

    Capitalize all names (#199)
    Hvis du har JAN OLA som fornavn fra PDL, ble det bare Jan ola med gammel logikk. Dette håndterer fortsatt ikke vanskelige caser som McCarty og lignende navn.

--- src/main/kotlin/no/nav/syfo/util/StringUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/StringUtil.kt
@@ -3,6 +3,8 @@ package no.nav.syfo.util
 import java.util.*
 
 fun String.lowerCapitalize() =
-    this.lowercase(Locale.getDefault()).replaceFirstChar {
-        if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+    this.split(" ").joinToString(" ") { navn ->
+        navn.lowercase(Locale.getDefault()).replaceFirstChar {
+            if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
+        }
     }
