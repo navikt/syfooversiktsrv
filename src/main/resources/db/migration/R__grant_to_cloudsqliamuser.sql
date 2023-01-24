aff7c3f520dfb831e0ec7387b569b8fb35be7241
geir-waagboe <geir.arne.waagbo@nav.no>
2023-01-24 11:44:05 +0100

    Grant select to dvh

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -1,6 +1,14 @@
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO "isyfo-analyse";
+DO $$
+BEGIN
+  CREATE USER "disykefravar-x4wt";
+  EXCEPTION WHEN DUPLICATE_OBJECT THEN
+  RAISE NOTICE 'not creating role disykefravar-x4wt -- it already exists';
+END
+$$;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO "disykefravar-x4wt";
 
 -- GRANT SELECT, UPDATE ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 -- GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
