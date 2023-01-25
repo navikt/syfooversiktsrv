234b19709d6c05dcf64b5634dcba37d2fb0261c9
Eirik Dahlen <dahleneirik@gmail.com>
2023-01-25 12:57:36 +0100

    Update sistendret in db on identhendelse

--- src/main/kotlin/no/nav/syfo/identhendelse/database/IdenthendelseQuery.kt
+++ src/main/kotlin/no/nav/syfo/identhendelse/database/IdenthendelseQuery.kt
@@ -2,20 +2,24 @@ package no.nav.syfo.identhendelse.database
 
 import no.nav.syfo.application.database.DatabaseInterface
 import no.nav.syfo.domain.PersonIdent
+import java.sql.Timestamp
+import java.time.Instant
 
 const val queryUpdatePersonOversiktStatusFnr =
     """
         UPDATE PERSON_OVERSIKT_STATUS
-        SET fnr = ?
+        SET fnr = ?, sist_endret = ?
         WHERE fnr = ?
     """
 
 fun DatabaseInterface.updatePersonOversiktStatusFnr(nyPersonident: PersonIdent, gammelPersonident: PersonIdent): Int {
     var updatedRows: Int
+    val now = Timestamp.from(Instant.now())
     this.connection.use { connection ->
         updatedRows = connection.prepareStatement(queryUpdatePersonOversiktStatusFnr).use {
             it.setString(1, nyPersonident.value)
-            it.setString(2, gammelPersonident.value)
+            it.setTimestamp(2, now)
+            it.setString(3, gammelPersonident.value)
             it.executeUpdate()
         }.also {
             connection.commit()
