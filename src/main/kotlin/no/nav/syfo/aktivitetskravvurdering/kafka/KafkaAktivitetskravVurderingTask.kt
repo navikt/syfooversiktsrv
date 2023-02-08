be8cb88d428ee37fee13e92160d76722e172dff2
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2023-02-08 16:05:45 +0100

    Reduce poll size (#236)

--- src/main/kotlin/no/nav/syfo/aktivitetskravvurdering/kafka/KafkaAktivitetskravVurderingTask.kt
+++ src/main/kotlin/no/nav/syfo/aktivitetskravvurdering/kafka/KafkaAktivitetskravVurderingTask.kt
@@ -21,6 +21,7 @@ fun launchKafkaTaskAktivitetskravVurdering(
     )
     val consumerProperties = Properties().apply {
         putAll(kafkaAivenConsumerConfig(kafkaEnvironment = kafkaEnvironment))
+        this[ConsumerConfig.MAX_POLL_RECORDS_CONFIG] = "10"
         this[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = KafkaAktivitetskravVurderingDeserializer::class.java
     }
 
