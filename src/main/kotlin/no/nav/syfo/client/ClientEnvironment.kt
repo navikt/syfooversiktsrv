2824e01c50fd74239a5fdafd04a966cba12b4282
geir-waagboe <geir.arne.waagbo@nav.no>
2023-03-10 10:17:30 +0100

    Revert "Repair aktivitetskrav kandidater (#258)"
    This reverts commit 8d863bb5ea207a5fffa7f671c3c13fe002739eaf.

--- src/main/kotlin/no/nav/syfo/client/ClientEnvironment.kt
+++ src/main/kotlin/no/nav/syfo/client/ClientEnvironment.kt
@@ -2,7 +2,6 @@ package no.nav.syfo.client
 
 data class ClientsEnvironment(
     val ereg: ClientEnvironment,
-    val oppfolgingstilfelle: ClientEnvironment,
     val pdl: ClientEnvironment,
     val syfobehandlendeenhet: ClientEnvironment,
     val syfotilgangskontroll: ClientEnvironment,
