c78dd99d6979520cbafa589295ff19781e723aff
Anders Rognstad <anders.rognstad@nav.no>
2023-03-23 10:16:59 +0100

    Create test for kafka aktivitetskrav consumer
    Fix date
    
    
    Fix database connection use

--- src/main/kotlin/no/nav/syfo/util/DateUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/DateUtil.kt
@@ -1,6 +1,7 @@
 package no.nav.syfo.util
 
 import java.time.*
+import java.time.temporal.ChronoUnit
 
 val defaultZoneOffset: ZoneOffset = ZoneOffset.UTC
 
@@ -8,5 +9,6 @@ fun nowUTC(): OffsetDateTime = OffsetDateTime.now(defaultZoneOffset)
 
 fun OffsetDateTime.toLocalDateOslo(): LocalDate = atZoneSameInstant(ZoneId.of("Europe/Oslo")).toLocalDate()
 fun OffsetDateTime.toLocalDateTimeOslo(): LocalDateTime = atZoneSameInstant(ZoneId.of("Europe/Oslo")).toLocalDateTime()
+fun OffsetDateTime.millisekundOpplosning(): OffsetDateTime = this.truncatedTo(ChronoUnit.MILLIS)
 
 fun LocalDate.isBeforeOrEqual(anotherDate: LocalDate): Boolean = (this == anotherDate || this.isBefore(anotherDate))
