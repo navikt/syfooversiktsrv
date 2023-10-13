94ace9eb8cc155ac55af9d375cdbef0e9a9ed8d5
Eirik Dahlen <dahleneirik@gmail.com>
2023-10-13 10:27:18 +0200

    IS-1651: Behandle møtebehov that is actually behandlet
    JML har gravd litt i dette også, men ikke funnet ut hvorfor det ikke ble oppdatert riktig hos oss. Vi får holde et øye med det.
    Se jirasaker:
    - https://jira.adeo.no/browse/FAGSYSTEM-295563
    - https://jira.adeo.no/browse/FAGSYSTEM-295879

---
+++ src/main/resources/db/migration/V15_4__close_motebehov.sql
@@ -0,0 +1,7 @@
+UPDATE person_oversikt_status
+SET motebehov_ubehandlet = FALSE
+WHERE uuid = '163ea536-be1f-4e51-9987-a74965bb8f11';
+
+UPDATE person_oversikt_status
+SET motebehov_ubehandlet = FALSE
+WHERE uuid = '9090ba90-3cda-4431-856d-90a9601c7576';
