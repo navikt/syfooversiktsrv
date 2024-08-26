5a43f4f4738c66e83d9b28ce23d4b691d477a279
Anders Rognstad <anders.rognstad@nav.no>
2024-08-26 08:26:09 +0200

    IS-2596: Reverter script

--- src/main/resources/db/migration/V17_4__migrate_is_aktiv_aktivitetskrav_vurdering.sql
+++
@@ -1,3 +0,0 @@
-UPDATE PERSON_OVERSIKT_STATUS
-SET is_aktiv_aktivitetskrav_vurdering =
-    (aktivitetskrav IN ('NY', 'AVVENT','NY_VURDERING', 'FORHANDSVARSEL') AND aktivitetskrav_stoppunkt > '2023-03-10');
