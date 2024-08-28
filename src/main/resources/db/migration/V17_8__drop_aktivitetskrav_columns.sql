e5e7fc583e491625f11a1ce510c509841fd9e777
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2024-08-28 12:56:37 +0200

    IS-2596: Drop aktivitetskrav columns no longer used (#424)

---
+++ src/main/resources/db/migration/V17_8__drop_aktivitetskrav_columns.sql
@@ -0,0 +1,5 @@
+ALTER TABLE person_oversikt_status
+    DROP COLUMN aktivitetskrav,
+    DROP COLUMN aktivitetskrav_stoppunkt,
+    DROP COLUMN aktivitetskrav_vurdering_frist,
+    DROP COLUMN aktivitetskrav_vurder_stans_ubehandlet;
