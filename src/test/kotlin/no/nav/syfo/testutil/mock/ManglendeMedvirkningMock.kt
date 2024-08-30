ee06332d5415f77f0930384f51b191ca29f9dcca
geir-waagboe <geir.arne.waagbo@nav.no>
2024-08-30 13:58:19 +0200

    IS-2595: Bruke ismanglendemedvirkning

--- src/test/kotlin/no/nav/syfo/testutil/mock/ManglendeMedvirkningMock.kt
+++ src/test/kotlin/no/nav/syfo/testutil/mock/ManglendeMedvirkningMock.kt
@@ -1,57 +1,35 @@
 package no.nav.syfo.testutil.mock
 
-import io.ktor.server.application.call
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import io.ktor.server.response.respond
-import io.ktor.server.routing.*
-import no.nav.syfo.personstatus.api.v2.auth.installContentNegotiation
+import io.ktor.client.engine.mock.*
+import io.ktor.client.request.*
 import no.nav.syfo.personstatus.application.manglendemedvirkning.ManglendeMedvirkningDTO
 import no.nav.syfo.personstatus.application.manglendemedvirkning.ManglendeMedvirkningResponseDTO
 import no.nav.syfo.personstatus.application.manglendemedvirkning.ManglendeMedvirkningVarselDTO
 import no.nav.syfo.personstatus.application.manglendemedvirkning.ManglendeMedvirkningVurderingType
-import no.nav.syfo.personstatus.infrastructure.clients.arbeidsuforhet.ManglendeMedvirkningClient
 import no.nav.syfo.testutil.UserConstants
-import no.nav.syfo.testutil.getRandomPort
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
 
-class ManglendeMedvirkningMock {
-    private val port = getRandomPort()
-    val url = "http://localhost:$port"
-
-    val name = "ismanglendemedvirkning"
-
-    val server = embeddedServer(
-        factory = Netty,
-        port = port,
-    ) {
-        installContentNegotiation()
-        routing {
-            post(ManglendeMedvirkningClient.MANGLENDE_MEDVIRKNING_API_PATH) {
-                call.respond(
-                    ManglendeMedvirkningResponseDTO(
-                        mapOf(
-                            Pair(
-                                UserConstants.ARBEIDSTAKER_FNR,
-                                ManglendeMedvirkningDTO(
-                                    uuid = UUID.randomUUID(),
-                                    personident = UserConstants.ARBEIDSTAKER_FNR,
-                                    createdAt = LocalDateTime.now().minusDays(1),
-                                    type = ManglendeMedvirkningVurderingType.FORHANDSVARSEL,
-                                    begrunnelse = "begrunnelse",
-                                    varsel = ManglendeMedvirkningVarselDTO(
-                                        uuid = UUID.randomUUID(),
-                                        createdAt = LocalDateTime.now().minusDays(1),
-                                        svarfrist = LocalDate.now().plusDays(1),
-                                    )
-                                ),
-                            ),
-                        ),
+fun MockRequestHandleScope.manglendeMedvirkningMockResponse(): HttpResponseData =
+    respondOk(
+        ManglendeMedvirkningResponseDTO(
+            mapOf(
+                Pair(
+                    UserConstants.ARBEIDSTAKER_FNR,
+                    ManglendeMedvirkningDTO(
+                        uuid = UUID.randomUUID(),
+                        personident = UserConstants.ARBEIDSTAKER_FNR,
+                        createdAt = LocalDateTime.now().minusDays(1),
+                        type = ManglendeMedvirkningVurderingType.FORHANDSVARSEL,
+                        begrunnelse = "begrunnelse",
+                        varsel = ManglendeMedvirkningVarselDTO(
+                            uuid = UUID.randomUUID(),
+                            createdAt = LocalDateTime.now().minusDays(1),
+                            svarfrist = LocalDate.now().plusDays(1),
+                        )
                     ),
-                )
-            }
-        }
-    }
-}
+                ),
+            ),
+        )
+    )
