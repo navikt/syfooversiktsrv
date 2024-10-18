cf13cd4f6057302dbcc635c457046c033c89ad70
geir-waagboe <geir.arne.waagbo@nav.no>
2024-10-18 16:06:39 +0200

    IS-2735: Nullstill utdaterte motesvar-oppgaver

---
+++ src/main/resources/db/migration/V18_00__close_outdated_motesvar.sql
@@ -0,0 +1,5 @@
+UPDATE person_oversikt_status SET dialogmotesvar_ubehandlet='f' WHERE uuid IN (
+'3f9b7a9a-8ff0-48f2-9a0b-968f37abb9d3',
+'861ef0a2-8a89-4146-b83f-12a5cc8a66a4',
+'3e571688-dfc3-466d-a30f-14517c3db492',
+'c3d945b3-b9fe-4dd2-b5f4-6703ee145c28');
