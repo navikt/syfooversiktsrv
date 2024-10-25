47450c044b471a43b8495e51b2025d4356837906
mariusfoss <marius.syverstad.fossum@nav.no>
2024-10-25 13:14:40 +0200

    Fikset tester som feiler på windows samt gjort noe refaktorering (#445)

--- src/test/kotlin/no/nav/syfo/testutil/assertion/PPersonOppfolgingstilfelleVirksomhetAssertion.kt
+++ src/test/kotlin/no/nav/syfo/testutil/assertion/PPersonOppfolgingstilfelleVirksomhetAssertion.kt
@@ -5,22 +5,32 @@ import no.nav.syfo.personstatus.domain.PPersonOppfolgingstilfelleVirksomhet
 import no.nav.syfo.testutil.mock.eregOrganisasjonResponse
 import org.amshove.kluent.shouldBeEqualTo
 import org.amshove.kluent.shouldBeNull
+import org.amshove.kluent.shouldBeTrue
 
 fun checkPPersonOppfolgingstilfelleVirksomhet(
     pPersonOppfolgingstilfelleVirksomhetList: List<PPersonOppfolgingstilfelleVirksomhet>,
     kafkaOppfolgingstilfellePerson: KafkaOppfolgingstilfellePerson,
-    updated: Boolean,
 ) {
     val virksomhetsnummerList = kafkaOppfolgingstilfellePerson.oppfolgingstilfelleList.first().virksomhetsnummerList
 
     pPersonOppfolgingstilfelleVirksomhetList.size shouldBeEqualTo virksomhetsnummerList.size
 
     pPersonOppfolgingstilfelleVirksomhetList.forEachIndexed { index, pPersonOppfolgingstilfelleVirksomhet ->
-        pPersonOppfolgingstilfelleVirksomhet.virksomhetsnummer.value shouldBeEqualTo virksomhetsnummerList[index]
-        if (updated) {
-            pPersonOppfolgingstilfelleVirksomhet.virksomhetsnavn shouldBeEqualTo eregOrganisasjonResponse.navn.redigertnavn
-        } else {
-            pPersonOppfolgingstilfelleVirksomhet.virksomhetsnavn.shouldBeNull()
-        }
+        virksomhetsnummerList.contains(pPersonOppfolgingstilfelleVirksomhet.virksomhetsnummer.value).shouldBeTrue()
+        pPersonOppfolgingstilfelleVirksomhet.virksomhetsnavn.shouldBeNull()
+    }
+}
+
+fun checkPPersonOppfolgingstilfelleVirksomhetUpdated(
+    pPersonOppfolgingstilfelleVirksomhetList: List<PPersonOppfolgingstilfelleVirksomhet>,
+    kafkaOppfolgingstilfellePerson: KafkaOppfolgingstilfellePerson,
+) {
+    val virksomhetsnummerList = kafkaOppfolgingstilfellePerson.oppfolgingstilfelleList.first().virksomhetsnummerList
+
+    pPersonOppfolgingstilfelleVirksomhetList.size shouldBeEqualTo virksomhetsnummerList.size
+
+    pPersonOppfolgingstilfelleVirksomhetList.forEach { pPersonOppfolgingstilfelleVirksomhet ->
+        virksomhetsnummerList.contains(pPersonOppfolgingstilfelleVirksomhet.virksomhetsnummer.value).shouldBeTrue()
+        pPersonOppfolgingstilfelleVirksomhet.virksomhetsnavn shouldBeEqualTo eregOrganisasjonResponse.navn.redigertnavn
     }
 }
