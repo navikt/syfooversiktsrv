3d2835d8005e3203156c50b22d23206e8ef040da
geir-waagboe <geir.arne.waagbo@nav.no>
2024-10-28 15:10:27 +0100

    IS-2711: Historikk for veilederknytning

--- src/main/resources/db/migration/V18_01__add_veileder_historikk.sql
+++ src/main/resources/db/migration/V18_01__add_veileder_historikk.sql
@@ -12,6 +12,6 @@ CREATE TABLE VEILEDER_HISTORIKK (
 CREATE INDEX IX_VEILEDER_HISTORIKK_STATUS_ID ON VEILEDER_HISTORIKK (person_oversikt_status_id);
 
 INSERT INTO VEILEDER_HISTORIKK (uuid,person_oversikt_status_id,tildelt_dato,tildelt_veileder,tildelt_enhet)
-SELECT gen_random_uuid(),id,oppfolgingstilfelle_start,tildelt_veileder,tildelt_enhet
+SELECT gen_random_uuid(),now(),id,oppfolgingstilfelle_start,tildelt_veileder,tildelt_enhet
 FROM PERSON_OVERSIKT_STATUS
 WHERE tildelt_veileder IS NOT NULL AND tildelt_enhet IS NOT NULL;
