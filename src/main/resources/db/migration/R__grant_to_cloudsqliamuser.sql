1fe6a3b5b0554bf4380449dc04cc1c992de29766
geir-waagboe <geir.arne.waagbo@nav.no>
2024-10-28 14:22:53 +0100

    Fix test-db

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -1,3 +1,3 @@
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO "isyfo-analyse";
