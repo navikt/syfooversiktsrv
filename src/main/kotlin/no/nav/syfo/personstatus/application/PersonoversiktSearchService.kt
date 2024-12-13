32a51d9f5d03797acca64958ddbb87476f32e546
Solgård <vetle.bu.solgard@nav.no>
2024-12-13 14:18:21 +0100

    IS-2871: Add possibility to search on name

--- src/main/kotlin/no/nav/syfo/personstatus/application/PersonoversiktSearchService.kt
+++ src/main/kotlin/no/nav/syfo/personstatus/application/PersonoversiktSearchService.kt
@@ -1,7 +1,7 @@
 package no.nav.syfo.personstatus.application
 
 import no.nav.syfo.personstatus.domain.PersonOversiktStatus
-import no.nav.syfo.personstatus.domain.SearchQuery
+import no.nav.syfo.personstatus.domain.Search
 import org.slf4j.LoggerFactory
 import kotlin.jvm.java
 import kotlin.time.measureTimedValue
@@ -9,9 +9,9 @@ import kotlin.time.measureTimedValue
 class PersonoversiktSearchService(
     private val personoversiktStatusRepository: IPersonOversiktStatusRepository,
 ) {
-    fun searchSykmeldt(searchQuery: SearchQuery): List<PersonOversiktStatus> {
+    fun searchSykmeldt(search: Search): List<PersonOversiktStatus> {
         val (searchResult, duration) = measureTimedValue {
-            personoversiktStatusRepository.searchPerson(searchQuery = searchQuery)
+            personoversiktStatusRepository.searchPerson(search = search)
         }
 
         log.info("Completed search for sykmeldt in repository, got ${searchResult.size} results in ${duration.inWholeMilliseconds} ms")
