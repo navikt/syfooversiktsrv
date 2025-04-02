3c337eef8efba6b007e43b09f8b2f5d26d763d75
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2025-04-02 15:26:14 +0200

    Oppfolgingsenhet nulling (#530)

--- src/main/kotlin/no/nav/syfo/personstatus/infrastructure/cronjob/reaper/ReaperCronjob.kt
+++ src/main/kotlin/no/nav/syfo/personstatus/infrastructure/cronjob/reaper/ReaperCronjob.kt
@@ -3,12 +3,24 @@ package no.nav.syfo.personstatus.infrastructure.cronjob.reaper
 import net.logstash.logback.argument.StructuredArguments
 import no.nav.syfo.personstatus.application.PersonoversiktStatusService
 import no.nav.syfo.personstatus.domain.PersonIdent
+import no.nav.syfo.personstatus.infrastructure.clients.behandlendeenhet.BehandlendeEnhetClient
 import no.nav.syfo.personstatus.infrastructure.cronjob.Cronjob
 import no.nav.syfo.personstatus.infrastructure.cronjob.CronjobResult
 import org.slf4j.LoggerFactory
+import java.util.*
 
+/**
+* Formålet med denne cronjob'en er å nulle ut veilederknytning for personer som ikke lengre er under
+* oppfølging. Problemet som løses er at vi ikke ønsker at personer som blir syke på nytt tar med seg
+* en veilederknytning fra et tidligere oppfølgingstilfelle (veilederen kan ha sluttet eller flyttet
+* til en annen enhet, eller personen kan ha flyttet i mellomtiden).
+* *Desember 2024:* Endrer slik at veilederknytningen nulles når det har gått 2 måneder (fra 3) siden siste
+* oppfølgingstilfelle-end og minst 2 måneder siden siste oppdatering.
+* *Mars 2025:* Utvider til også å nulle oppfølgingsenhet
+*/
 class ReaperCronjob(
     private val personOversiktStatusService: PersonoversiktStatusService,
+    private val behandlendeEnhetClient: BehandlendeEnhetClient,
 ) : Cronjob {
     private val log = LoggerFactory.getLogger(ReaperCronjob::class.java)
 
@@ -19,27 +31,23 @@ class ReaperCronjob(
         runJob()
     }
 
-    fun runJob(): CronjobResult {
+    suspend fun runJob(): CronjobResult {
         log.info("Run ReaperCronjob")
         val result = CronjobResult()
 
-        /*
-        Formålet med denne cronjob'en er å nulle ut veilederknytning for personer som ikke lengre er under
-        oppfølging. Problemet som løses er at vi ikke ønsker at personer som blir syke på nytt tar med seg
-        en veilederknytning fra et tidligere oppfølgingstilfelle (veilederen kan ha sluttet eller flyttet
-        til en annen enhet, eller personen kan ha flyttet i mellomtiden).
-        Desember 2024: Endrer slik at veilederknytningen nulles når det har gått 2 måneder (fra 3) siden siste
-        oppfølgingstilfelle-end og minst 2 måneder siden siste oppdatering.
-         */
         personOversiktStatusService.getPersonerWithVeilederTildelingAndOldOppfolgingstilfelle()
             .forEach {
                 try {
                     personOversiktStatusService.removeTildeltVeileder(PersonIdent(it.fnr))
+                    behandlendeEnhetClient.unsetOppfolgingsenhet(
+                        callId = UUID.randomUUID().toString(),
+                        personIdent = PersonIdent(it.fnr),
+                    )
                     result.updated++
                     COUNT_CRONJOB_REAPER_UPDATE.increment()
                 } catch (ex: Exception) {
                     log.error(
-                        "Exception caught while attempting to remove tildelt veileder",
+                        "Exception caught while attempting to remove tildelt veileder and oppfolgingsenhet",
                         ex
                     )
                     result.failed++
