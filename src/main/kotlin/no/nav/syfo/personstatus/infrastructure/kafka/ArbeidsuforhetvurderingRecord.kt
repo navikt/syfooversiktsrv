920d0f456336c69af487123ed33b414d081e98c0
Vetle Bu Solgård <vetle.bu.solgard@nav.no>
2025-08-18 12:21:36 +0200

    IS: Removing unused properties from record + not handling unused enums (#574)

--- src/main/kotlin/no/nav/syfo/personstatus/infrastructure/kafka/ArbeidsuforhetvurderingRecord.kt
+++ src/main/kotlin/no/nav/syfo/personstatus/infrastructure/kafka/ArbeidsuforhetvurderingRecord.kt
@@ -1,20 +1,9 @@
 package no.nav.syfo.personstatus.infrastructure.kafka
 
-import java.time.LocalDate
-import java.time.OffsetDateTime
 import java.util.*
 
 data class ArbeidsuforhetvurderingRecord(
     val uuid: UUID,
-    val createdAt: OffsetDateTime,
     val personident: String,
-    val veilederident: String,
-    val type: VurderingType,
-    val begrunnelse: String,
-    val gjelderFom: LocalDate?,
     val isFinal: Boolean,
 )
-
-enum class VurderingType {
-    FORHANDSVARSEL, OPPFYLT, AVSLAG, IKKE_AKTUELL, AVSLAG_UTEN_FORHANDSVARSEL, OPPFYLT_UTEN_FORHANDSVARSEL
-}
