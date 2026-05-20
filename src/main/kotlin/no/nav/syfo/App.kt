32d8dc7ec30f96b5a471311703c4af49f8ff5f55
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2026-05-20 10:28:40 +0200

    IS-3953: Forbedre shutdown-hook (#626)

--- src/main/kotlin/no/nav/syfo/App.kt
+++ src/main/kotlin/no/nav/syfo/App.kt
@@ -36,7 +36,6 @@ import redis.clients.jedis.DefaultJedisClientConfig
 import redis.clients.jedis.HostAndPort
 import redis.clients.jedis.JedisPool
 import redis.clients.jedis.JedisPoolConfig
-import java.util.concurrent.TimeUnit
 
 const val applicationPort = 8080
 
@@ -216,7 +215,7 @@ fun main() {
 
     Runtime.getRuntime().addShutdownHook(
         Thread {
-            server.stop(10, 10, TimeUnit.SECONDS)
+            applicationState.ready = false
         }
     )
 
