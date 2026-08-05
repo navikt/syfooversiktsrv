383c1706ad6368a4a31bc97cf521c475819d1ac6
geir-waagboe <geir.arne.waagbo@nav.no>
2026-08-05 15:44:09 +0200

    Temporary

--- src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
+++ src/main/resources/db/migration/R__grant_to_cloudsqliamuser.sql
@@ -1,4 +1,4 @@
 REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO "isyfo-analyse";
 -- touch
